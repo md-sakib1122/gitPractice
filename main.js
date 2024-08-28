6effc67fd4ee321d232cb9adb6c0e49dfc5fc51e
Sakib Hossen <mdaskib5566@gmail.com>
2024-08-28 22:32:37 +0600

    hiiii

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-console.log("i am in the main branch");
-console.log("adding form branch1");
-console.log("hiii");
-console.log("adding form branch1");
-console.log("hiii");
-
-console.log("hiii");
-console.log("hixii");
+console.log('hi my name is sakib');
+console.log('hi my name is rakib');
